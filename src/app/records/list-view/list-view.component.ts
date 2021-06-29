751494cc647546c24fa7c4ba4d72e32eeef46107
leninriv <leninriv@gmail.com>
2021-06-29 18:36:07 -0500

    excel and filter fixes

--- src/app/records/list-view/list-view.component.ts
+++ src/app/records/list-view/list-view.component.ts
@@ -54,24 +54,53 @@ export class ListViewComponent implements OnInit {
     this.msg.success(item.email);
   }
 
-  setNameInCollection(name: string, collection: Array<any>) {
+  setNameInCollection(name: string, lastName: string, collection: Array<any>) {
     const newCollection = [];
     for (let item of collection) {
+      if (!item) { item = {}; }
       item.name = name;
+      item.last_name = lastName;
       newCollection.push(item);
     }
     return newCollection;
   }
 
-  changeSearch(query: any) {
+  changeSearch(query: string) {
     if (query && query.length) {
       const completeList = this.asambleaService.getList();
-      const filteredList = completeList.filter(item => item?.nombres?.includes(query) || item?.apellidos?.includes(query));
+      const filteredList = completeList.filter(item => item?.nombres?.toLowerCase().includes(query.toLowerCase()) || item?.apellidos?.toLowerCase().includes(query.toLowerCase()));
       this.list = filteredList;
     } else {
       this.list = this.asambleaService.getList();
     }
   }
+
+  advisersFix(advisers: Array<any>) {
+    const newAdvisersArray = []
+    const newAdvisers: any = {}
+    for (const item of advisers) {
+      if (newAdvisers[item.name + item.last_name]) {
+        newAdvisers[item.name + item.last_name].push(item)
+      } else {
+        newAdvisers[item.name + item.last_name] = [item]
+      }
+    }
+
+    for (let key of Object.keys(newAdvisers) ) {
+      newAdvisersArray.push(
+        {
+          name: newAdvisers[key][0].name,
+          last_name: newAdvisers[key][0].last_name,
+          asesor_1_nombre: newAdvisers[key][0].nombre,
+          asesor_1_contacto: newAdvisers[key][0].contacto,
+          asesor_2_nombre: newAdvisers[key][1]?.nombre || '-',
+          asesor_2_contacto: newAdvisers[key][1]?.contacto || '-',
+        }
+      )
+    }
+    return newAdvisersArray;
+  }
+
 
   jsonToExcel() {
     const allBlocks = ['academy', 'previous_elections', 'trayectory_service', 'companies_societies', 'shareholder_companies_societies', 'tax_declaration', 'taxes_foreign_currency', 'legal_proceedings_actor', 'legal_proceedings_defendant', 'alimony_pensions', 'advisers'];
@@ -83,13 +112,17 @@ export class ListViewComponent implements OnInit {
       const itemFields = cloneDeep(item);
       for (let key of allBlocks) {
         if (!newObjectList[key]) { newObjectList[key] = []; }
+
         if (item[key] && item[key].length && typeof item[key] !== 'string') {
-          newObjectList[key] = newObjectList[key].concat(this.setNameInCollection(item.nombres + ' ' + item.apellidos, item[key]))
+          newObjectList[key] = newObjectList[key].concat(this.setNameInCollection(item.nombres, item.apellidos, item[key]))
+        } else if (item[key] === 'No Registra') {
+          newObjectList[key] = newObjectList[key].concat(this.setNameInCollection(item.nombres, item.apellidos, [{ validation: 'No Registra' }]));
         }
         delete itemFields[key];
       }
       newObjectList.general.push(itemFields)
     }
+    newObjectList.advisers = this.advisersFix(newObjectList.advisers);
     this.excelService.exportAsExcelFile(newObjectList, 'asamblea');
   }
 }
